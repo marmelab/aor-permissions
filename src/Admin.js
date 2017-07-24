1315fe0b3741e889b2439de8bcb08d8cb36e28cb
gildas <gildas.garcia@gmail.com>
2017-07-24 12:25:44 +0200

    Use same permissions props for actions

--- src/Admin.js
+++ src/Admin.js
@@ -3,10 +3,47 @@ import PropTypes from 'prop-types';
 import { Admin as OriginalAdmin, Resource } from 'admin-on-rest';
 import AuthProvider from './AuthProvider';
 import { AUTH_GET_PERMISSIONS } from './constants';
+import { resolvePermission as defaultResolvePermission } from './resolvePermissions';
 
-export const applyPermissionsToResource = async (authClient, resource) => {
-    if (resource.props.permission && !await authClient(AUTH_GET_PERMISSIONS, resource.props.permission)) {
-        return false;
+export const defaultApplyPermissionsToAction = async ({
+    permissions,
+    resource,
+    action,
+    resolvePermission = defaultResolvePermission,
+}) => {
+    if (resource.props[`${action}Permissions`]) {
+        const match = await resolvePermission({ permissions, resource })({
+            exact: resource.props[`${action}Exact`],
+            permissions: resource.props[`${action}Permissions`],
+            resolve: resource.props[`${action}Resolve`],
+            view: true,
+        });
+
+        return match.matched ? resource.props[action] : null;
+    }
+
+    return resource.props[action];
+};
+
+export const defaultApplyPermissionsToResource = async ({
+    authClient,
+    resource,
+    resolvePermission = defaultResolvePermission,
+    applyPermissionsToAction = defaultApplyPermissionsToAction,
+}) => {
+    const permissions = await authClient(AUTH_GET_PERMISSIONS, { resource });
+
+    if (resource.props.permissions) {
+        const match = await resolvePermission({ permissions, resource })({
+            exact: resource.props.exact,
+            permissions: resource.props.permissions,
+            resolve: resource.props.resolve,
+            view: true,
+        });
+
+        if (!match.matched) {
+            return false;
+        }
     }
 
     const newResource = {
@@ -14,29 +51,21 @@ export const applyPermissionsToResource = async (authClient, resource) => {
         icon: resource.props.icon,
         options: resource.props.options,
         checkCredentials: resource.props.checkCredentials,
-        list: !resource.props.listPermission || (await authClient(AUTH_GET_PERMISSIONS, resource.props.listPermission))
-            ? resource.props.list
-            : null,
-        create: !resource.props.createPermission ||
-            (await authClient(AUTH_GET_PERMISSIONS, resource.props.createPermission))
-            ? resource.props.create
-            : null,
-        edit: !resource.props.editPermission || (await authClient(AUTH_GET_PERMISSIONS, resource.props.editPermission))
-            ? resource.props.edit
-            : null,
-        show: !resource.props.showPermission || (await authClient(AUTH_GET_PERMISSIONS, resource.props.showPermission))
-            ? resource.props.show
-            : null,
-        remove: !resource.props.removePermission ||
-            (await authClient(AUTH_GET_PERMISSIONS, resource.props.removePermission))
-            ? resource.props.remove
-            : null,
+        list: await applyPermissionsToAction({ permissions, resource, action: 'list' }),
+        create: await applyPermissionsToAction({ permissions, resource, action: 'create' }),
+        edit: await applyPermissionsToAction({ permissions, resource, action: 'edit' }),
+        show: await applyPermissionsToAction({ permissions, resource, action: 'show' }),
+        remove: await applyPermissionsToAction({ permissions, resource, action: 'remove' }),
     };
 
     return newResource;
 };
 
-export const applyPermissionsToResources = async (authClient, resources) => {
+export const applyPermissionsToResources = async ({
+    authClient,
+    resources,
+    applyPermissionsToResource = defaultApplyPermissionsToResource,
+}) => {
     const newResources = await Promise.all(
         Children.map(resources, resource => applyPermissionsToResource(authClient, resource)),
     );
@@ -53,9 +82,9 @@ export default class Admin extends Component {
     state = { resources: false };
 
     componentWillMount() {
-        applyPermissionsToResources(this.props.authClient, this.props.children).then(resources =>
-            this.setState({ resources }),
-        );
+        applyPermissionsToResources({ authClient: this.props.authClient, resources: this.props.children })
+            .then(resources => this.setState({ resources }))
+            .catch(error => console.error(error));
     }
 
     render() {
---
+++ src/Admin.spec.js
@@ -0,0 +1,150 @@
+import expect, { createSpy } from 'expect';
+
+import {
+    defaultApplyPermissionsToAction,
+    defaultApplyPermissionsToResource,
+    applyPermissionsToResources,
+} from './Admin';
+
+describe('<Admin>', () => {
+    describe('applyPermissionsToAction', () => {
+        it('resolves to the action component if no permissions attributes are defined', async () => {
+            const result = await defaultApplyPermissionsToAction({
+                permissions: [],
+                resource: { props: { list: 'listComponent' } },
+                action: 'list',
+            });
+
+            expect(result).toEqual('listComponent');
+        });
+
+        it('resolves to the action component if permissions attributes are defined and resolvePermission matches', async () => {
+            const resolvePermission = createSpy().andReturn(() => Promise.resolve({ matched: true }));
+
+            const result = await defaultApplyPermissionsToAction({
+                permissions: [],
+                resource: { props: { list: 'listComponent', listPermissions: 'foo' } },
+                action: 'list',
+                resolvePermission,
+            });
+
+            expect(result).toEqual('listComponent');
+        });
+
+        it('resolves to null if permissions attributes are defined and resolvePermission does not match', async () => {
+            const resolvePermission = createSpy().andReturn(() => Promise.resolve({ matched: false }));
+
+            const result = await defaultApplyPermissionsToAction({
+                permissions: [],
+                resource: { props: { list: 'listComponent', listPermissions: 'foo' } },
+                action: 'list',
+                resolvePermission,
+            });
+
+            expect(result).toEqual(null);
+        });
+    });
+
+    describe('defaultApplyPermissionsToResource', () => {
+        it('resolves to the resource if no permissions attributes are defined', async () => {
+            const authClient = createSpy().andReturn(Promise.resolve('foo'));
+            const result = await defaultApplyPermissionsToResource({
+                authClient,
+                resource: {
+                    props: {
+                        name: 'aResource',
+                        icon: 'anIcon',
+                        options: 'someOptions',
+                        checkCredentials: 'doesCheckCredentials',
+                        list: 'listComponent',
+                        create: 'createComponent',
+                        edit: 'editComponent',
+                        show: 'showComponent',
+                        remove: 'removeComponent',
+                    },
+                },
+            });
+
+            expect(result).toEqual({
+                name: 'aResource',
+                icon: 'anIcon',
+                options: 'someOptions',
+                checkCredentials: 'doesCheckCredentials',
+                list: 'listComponent',
+                create: 'createComponent',
+                edit: 'editComponent',
+                show: 'showComponent',
+                remove: 'removeComponent',
+            });
+        });
+
+        it('resolves to the resource with filtered actions if permissions attributes are defined', async () => {
+            const authClient = createSpy().andReturn(Promise.resolve(['list', 'show']));
+            const result = await defaultApplyPermissionsToResource({
+                authClient,
+                resource: {
+                    props: {
+                        name: 'aResource',
+                        icon: 'anIcon',
+                        options: 'someOptions',
+                        checkCredentials: 'doesCheckCredentials',
+                        list: 'listComponent',
+                        listPermissions: 'list',
+                        create: 'createComponent',
+                        createPermissions: 'foo',
+                        edit: 'editComponent',
+                        editPermissions: 'foo',
+                        show: 'showComponent',
+                        showPermissions: 'show',
+                        showResolve: () => Promise.resolve(false),
+                        remove: 'removeComponent',
+                        removePermissions: 'foo',
+                        removeResolve: () => true,
+                    },
+                },
+            });
+
+            expect(result).toEqual({
+                name: 'aResource',
+                icon: 'anIcon',
+                options: 'someOptions',
+                checkCredentials: 'doesCheckCredentials',
+                list: 'listComponent',
+                create: null,
+                edit: null,
+                show: null,
+                remove: 'removeComponent',
+            });
+        });
+
+        it('resolves to false if permissions attribute is defined and resolvePermission does not match', async () => {
+            const authClient = createSpy().andReturn(Promise.resolve('foo'));
+            const resolvePermission = createSpy().andReturn(() => Promise.resolve({ matched: false }));
+            const result = await defaultApplyPermissionsToResource({
+                authClient,
+                resource: { props: { permissions: 'bar' } },
+                resolvePermission,
+            });
+
+            expect(result).toEqual(false);
+        });
+    });
+
+    describe('applyPermissionsToResources', () => {
+        it('resolves to filtered resources', async () => {
+            const applyPermissionsToResource = createSpy().andCall(
+                (authClient, res) => (res === 'resource1' ? res : false),
+            );
+
+            const resources = await applyPermissionsToResources({
+                authClient: 'authClientImpl',
+                resources: ['resource1', 'resource2'],
+                applyPermissionsToResource,
+            });
+
+            expect(resources).toEqual(['resource1']);
+            expect(applyPermissionsToResource).toHaveBeenCalledWith('authClientImpl', 'resource1');
+            expect(applyPermissionsToResource).toHaveBeenCalledWith('authClientImpl', 'resource2');
+        });
+    });
+});
